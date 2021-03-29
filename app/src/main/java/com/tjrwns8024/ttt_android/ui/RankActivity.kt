966a0f544c9b8ea70701a00b6d962808d0e18959
윤석준 <tjrwns8024@naver.com>
2021-03-29 16:42:42 +0900

    [CREATE] RankActivity.kt

---
+++ app/src/main/java/com/tjrwns8024/ttt_android/ui/RankActivity.kt
@@ -0,0 +1,12 @@
+package com.tjrwns8024.ttt_android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tjrwns8024.ttt_android.R
+
+class RankActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rank)
+    }
+}

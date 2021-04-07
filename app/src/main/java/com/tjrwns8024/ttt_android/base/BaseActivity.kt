d236b3556a004caf6b9aa61cbd711b241e623c2f
윤석준 <tjrwns8024@naver.com>
2021-04-07 10:01:57 +0900

    [MODIFY] BaseActivity - setViewModel

--- app/src/main/java/com/tjrwns8024/ttt_android/base/BaseActivity.kt
+++ app/src/main/java/com/tjrwns8024/ttt_android/base/BaseActivity.kt
@@ -6,16 +6,27 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import com.tjrwns8024.ttt_android.BR
 
-abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding, E : BaseViewModel> : AppCompatActivity() {
 
     lateinit var binding: T
-
+    lateinit var viewModel: E
     abstract val layoutId: Int
+    abstract val viewModelStoreOwner: ViewModelStoreOwner
+    abstract val modelClass: Class<E>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutId)
+        viewModel = ViewModelProvider(viewModelStoreOwner).get(modelClass)
+
+        binding.setVariable(BR.vm, viewModel)
+        binding.lifecycleOwner = this
+
+        observeEvent()
 
         //전체화면
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
@@ -24,6 +35,7 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
             window.attributes.layoutInDisplayCutoutMode =
                 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
-
     }
+
+    abstract fun observeEvent()
 }

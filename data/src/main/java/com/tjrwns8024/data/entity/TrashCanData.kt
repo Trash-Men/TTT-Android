a7cdfff3848bbca7c7be80095e75f981499f108c
윤석준 <tjrwns8024@naver.com>
2021-04-20 14:42:56 +0900

    [ADD] <entity/TrashCanData.kt>

--- data/src/main/java/com/tjrwns8024/data/entity/TrashCanData.kt
+++ data/src/main/java/com/tjrwns8024/data/entity/TrashCanData.kt
@@ -1,39 +1,12 @@
 package com.tjrwns8024.data.entity
 
-import com.google.gson.annotations.SerializedName
-import com.tjrwns8024.domain.entity.TrashCan
-import com.tjrwns8024.domain.entity.TrashCanList
+import com.tjrwns8024.domain.entity.TrashList
 
 data class TrashCanListData(
-    val trashCans: List<TrashCanData>
+    val trashCans: List<TrashData>
 )
 
-data class TrashCanData(
-    @SerializedName("photo_url")
-    val photoURL: String,
-    @SerializedName("latitude")
-    val latitude: Float,
-    @SerializedName("longitude")
-    val longitude: Float,
-    @SerializedName("area")
-    val area: String,
-    @SerializedName("address")
-    val address: String,
-    @SerializedName("created_at")
-    val createdAt: String
-)
-
-fun TrashCanListData.toEntity(): TrashCanList =
-    TrashCanList(trashCans.map {
+fun TrashCanListData.toEntity(): TrashList =
+    TrashList(trashCans.map {
         it.toEntity()
     })
-
-fun TrashCanData.toEntity(): TrashCan =
-    TrashCan(
-        photoURL,
-        latitude,
-        longitude,
-        area,
-        address,
-        createdAt
-    )

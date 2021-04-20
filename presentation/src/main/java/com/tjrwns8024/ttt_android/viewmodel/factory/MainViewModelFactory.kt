0c6281ce402a7214bff49a1a0514b17f287ed885
윤석준 <tjrwns8024@naver.com>
2021-04-20 14:45:50 +0900

    move to package factory

--- presentation/src/main/java/com/tjrwns8024/ttt_android/viewmodel/factory/MainViewModelFactory.kt
+++ presentation/src/main/java/com/tjrwns8024/ttt_android/viewmodel/factory/MainViewModelFactory.kt
@@ -1,4 +1,4 @@
-package com.tjrwns8024.ttt_android.viewmodel
+package com.tjrwns8024.ttt_android.viewmodel.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider

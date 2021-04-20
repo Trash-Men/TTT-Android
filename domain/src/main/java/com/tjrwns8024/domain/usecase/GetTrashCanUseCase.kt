c237c42c10404900376ec79623db64b3f202eaff
윤석준 <tjrwns8024@naver.com>
2021-04-20 14:46:08 +0900

    [MODIFY] <usecase/ GetTrashCanUseCase.kt>

--- domain/src/main/java/com/tjrwns8024/domain/usecase/GetTrashCanUseCase.kt
+++ domain/src/main/java/com/tjrwns8024/domain/usecase/GetTrashCanUseCase.kt
@@ -1,7 +1,7 @@
 package com.tjrwns8024.domain.usecase
 
 import com.tjrwns8024.domain.base.UseCase
-import com.tjrwns8024.domain.entity.TrashCanList
+import com.tjrwns8024.domain.entity.TrashList
 import com.tjrwns8024.domain.repository.TrashRepository
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
@@ -9,7 +9,7 @@ import io.reactivex.disposables.CompositeDisposable
 class GetTrashCanUseCase(
     private val trashRepository: TrashRepository,
     compositeDisposable: CompositeDisposable
-) : UseCase<Unit, TrashCanList>(compositeDisposable) {
-    override fun createObservable(data: Unit): Single<TrashCanList> =
+) : UseCase<Unit, TrashList>(compositeDisposable) {
+    override fun createObservable(data: Unit): Single<TrashList> =
         trashRepository.getTrashCanList()
 }

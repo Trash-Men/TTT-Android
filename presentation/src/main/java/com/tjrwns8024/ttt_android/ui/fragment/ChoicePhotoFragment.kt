9fd0c1f6e0d86029e35315bdbd13c2afc7fe7eae
윤석준 <tjrwns8024@naver.com>
2021-04-21 11:03:43 +0900

    [ADD] <ChoicePhoto>

--- presentation/src/main/java/com/tjrwns8024/ttt_android/ui/fragment/ChoicePhotoFragment.kt
+++ presentation/src/main/java/com/tjrwns8024/ttt_android/ui/fragment/ChoicePhotoFragment.kt
@@ -4,21 +4,26 @@ import androidx.lifecycle.ViewModelProvider
 import com.tjrwns8024.ttt_android.R
 import com.tjrwns8024.ttt_android.base.BaseFragment
 import com.tjrwns8024.ttt_android.databinding.FragmentChoicePhotoBinding
-import com.tjrwns8024.ttt_android.viewmodel.MainViewModel
-import com.tjrwns8024.ttt_android.viewmodel.factory.PhotoViewModelFactory
-import javax.inject.Inject
+import com.tjrwns8024.ttt_android.viewmodel.PhotoViewModel
 
 class ChoicePhotoFragment : BaseFragment<FragmentChoicePhotoBinding>() {
 
-    @Inject
-    lateinit var factory: PhotoViewModelFactory
+//    @Inject
+//    lateinit var factory: PhotoViewModelFactory
 
     override val viewModel by lazy {
-        ViewModelProvider(this, factory).get(MainViewModel::class.java)
+        ViewModelProvider(this).get(PhotoViewModel::class.java)
     }
+
     override val layoutId: Int = R.layout.fragment_choice_photo
 
     override fun observeEvents() {
-
+        with(viewModel){
+            backEvent.observe(this@ChoicePhotoFragment, {
+                it.getContentIfNotHandled()?.let {
+                    requireActivity().finish()
+                }
+            })
+        }
     }
 }
--- presentation/src/main/java/com/tjrwns8024/ttt_android/viewmodel/PhotoViewModel.kt
+++ presentation/src/main/java/com/tjrwns8024/ttt_android/viewmodel/PhotoViewModel.kt
@@ -1,6 +1,13 @@
 package com.tjrwns8024.ttt_android.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import com.tjrwns8024.ttt_android.base.BaseViewModel
+import com.tjrwns8024.ttt_android.util.Event
 
-class PhotoViewModel(): BaseViewModel() {
+class PhotoViewModel : BaseViewModel() {
+    val backEvent = MutableLiveData<Event<Boolean>>()
+
+    fun clickBack() {
+        backEvent.value = Event(true)
+    }
 }

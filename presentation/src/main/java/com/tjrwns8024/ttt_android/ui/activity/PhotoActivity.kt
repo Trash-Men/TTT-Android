60ca6db963ecddb052c973e9f71df9769e512ef1
윤석준 <tjrwns8024@naver.com>
2021-04-26 21:36:02 +0900

    [ADD] Camera self Permission

--- presentation/src/main/java/com/tjrwns8024/ttt_android/ui/activity/PhotoActivity.kt
+++ presentation/src/main/java/com/tjrwns8024/ttt_android/ui/activity/PhotoActivity.kt
@@ -1,7 +1,16 @@
 package com.tjrwns8024.ttt_android.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.tjrwns8024.ttt_android.R
 
 class PhotoActivity : AppCompatActivity() {
@@ -9,6 +18,16 @@ class PhotoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
 
-
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                201
+            )
+        }
     }
 }
